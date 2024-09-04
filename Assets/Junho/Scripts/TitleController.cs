3848c4e870df9009c4db8d1437d1d4fe0ec6931a
Junho Mun <124476542+juknow@users.noreply.github.com>
2024-09-04 23:26:40 +0900

    [feat] 패배씬 추가 , 애니메이션 추가
    애니메이션은 부울 로직만

--- Assets/Junho/Scripts/TitleController.cs
+++ Assets/Junho/Scripts/TitleController.cs
@@ -1,12 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class TitleController : MonoBehaviour
 {
-    private Vector3 hoverScale = new Vector3(1.2f, 1.2f, 1.2f);  // 마우스 올렸을 때 크기
-    private Vector3 originalScale;  // 원래 크기
     // Start is called before the first frame update
     void Start()
     {
@@ -19,18 +15,15 @@ public class TitleController : MonoBehaviour
 
     }
 
-    public void OnPointerEnter()
+    public void StarGameButton()
     {
-        // 마우스가 버튼 위로 올라갈 때 버튼 크기를 확대
-        transform.localScale = hoverScale;
-        Debug.Log("gksms");
+        SceneManager.LoadScene(1);
+
     }
 
-    public void OnPointerExit()
+    public void ExitGameButton()
     {
-        // 마우스가 버튼을 벗어났을 때 원래 크기로 복구
-        transform.localScale = originalScale;
-        Debug.Log("gksms");
+        Application.Quit();
     }
 }
 
