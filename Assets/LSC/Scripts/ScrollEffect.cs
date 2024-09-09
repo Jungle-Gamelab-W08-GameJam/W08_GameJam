854022f3eae49104e7c394f38e111f155553b5a8
lsc892 <extraordinary2549@gmail.com>
2024-09-09 23:04:08 +0900

    [Add] 주문서 이펙트 추가, 버그 수정

---
+++ Assets/LSC/Scripts/ScrollEffect.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollEffect : MonoBehaviour
+{
+    public void DestoryEff() => Destroy(gameObject);
+}
